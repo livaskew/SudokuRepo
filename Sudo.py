5424a70d79305c484f1959771bd05ed649b515eb
livaskew <oliviaaskew@gmail.com>
2019-10-22 11:18:04 -0700

    check row, col, box added

--- Sudo.py
+++ Sudo.py
@@ -10,45 +10,70 @@
   #  c) Else, remove digit and try another
  # If all digits have been tried and nothing worked, return false
 
+ import collections
+ from collections import deque
 
-#need to define the thing that solves the sudoku, and basically defines the grid
-def solve(a):
-	for i in range (9)
-	for j in range (9)
+
 #need to define the thing that searches for empty boxes, still working on this
-def findZero (a,1):
-	for row in range (9):
-	for col in range (9):
-#i dont understand this next line, revisit    
- if (a [row] [col] == 0):
- 	1[0]= row
-	1[1]= col
-	return True
-return False
+def findZero (a,t):
+    for row in range (9):
+        for col in range (9):
+            if (a [row] [col] == 0):
+                 t [0]= row
+                 t [1]= col
+                 t [2]= int
+                return True
+    return False
 
-#need to define the thing that sets constraints for unique integers range(9) per row 
-def rowFind(a):
-	for i in range(9), a[1] != a[i+1]
+#check row, will return t or f whether or not a value has be used in a row
+def usedRow (a, row, num)
+    for i in range(9):
+        if (a [row] [i] == num):
+            return True
+    return False
+#check col, will return t or f whether or not a value had been used in a col
+def usedCol (a, col, num)
+    for i in range(9):
+        if (a [i][col] == num)
+            return True
+    return False
 
-#need to define the thing that sets constraints for unique integers range(9) per col
-def colFind(a):
-	for j in range(9), a[j] != a[j+1]
-#need to define the thing that sets constraints for unique integers range(9) for subboxes
+#check box, will return t or f whethor or not a vlaue has been used in a subbox
+def usedBox (a, row, col, num)
+    for i in range(3):
+        for j in range(3):
+            if (a [row+1][col+j] === num):
+                return True
+        return False
 
-#need to define elif statement for each that will remove integer if its not working. this is the backtracking piece
+
+#need to define the thing that sets constraints for unique integers range(9) per row/col/subboxes
+# and set integer and recursivly fill grid
+def findFill(a):
+    for i in range(9), a[0] != a[i+1]
+    for j in range(9), a[0] != a[j+1]
+#set up subboxes?
+
+#need to define the thing that solves the sudoku, this is the MAIN PROGRAM
+
+def rsolve
+    t=[0,0, int]
+#using findZero, check if there are empty things. if there are empty things, return true and keep going
+    if (not(findZero(a,t))):
+        return True;
+#consider digits 1 to 9
+    for t for row in range(9)
+    for t for col in range(9)
+
+
+
+
 
 #need to set up bool if grid is solved then true and if grid cannot be solved then false.
 
+#this is what t is for def findZero the recursive solve function, it tracks which empty cell we are at
+t.append[int, [i, j]]
 
-
-
-
-
-
-
-
- if i != 0 return True
-	elif return False
 
 #Ians liv.py
 
@@ -62,12 +87,12 @@ a = [ [ 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [ 0, 0, 0, 0, 0, 0, 0, 0, 0], 
       [ 0, 0, 0, 0, 0, 0, 0, 0, 0] ]
 
-solve (a)
+
 
 print( "\n +---+---+---+---+---+---+---+---+---+" )
 
 [ print( " |", row[i], end='' ) if i != 8
-	else print( " |", row[i], "|\n +---+---+---+---+---+---+---+---+---+" )
-	for row in a for i in range (len(row) ) ]
+else print( " |", row[i], "|\n +---+---+---+---+---+---+---+---+---+" )
+for row in a for i in range (len(row) ) ]
 
  
